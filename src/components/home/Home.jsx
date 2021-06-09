7bbb0c22904f1eb641bf962b32ba351f4f9ad200
mohamed18yo <mohamed18yosef@gmail.com>
2021-06-09 12:58:32 +0300

    mack it better

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import Button from "../elements/butoon/Butoon";
 import { v4 as uuidv4 } from "uuid";
 import ItemList from "../elements/itemList/ItemList";
@@ -7,99 +7,103 @@ import "./style.css";
 import { Data } from "../../data/Data";
 import axios from "axios";
 
-class Home extends Component {
-  state = {
-    value: "",
-    list: [],
-    error: "",
+function Home() {
+  // old way to use stat :
+  // const [state, setState] = useState({
+  //   value: "",
+  //   list: [],
+  //   error: "",
+  // });
+  // new way to use stat :
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+
+      setList(res.data.splice(0, 5));
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  componentDidMount() {
-
-    // async = promis 
-    // by async 
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          "https://jsonplaceholder.typicode.com/todos"
-        );
-       
-        this.setState({
-          list: res.data.splice(0,5) 
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  useEffect(() => {
     fetchData();
-
-    //by promis 
-
-    // axios.get('https://jsonplaceholder.typicode.com/todos').then(res=>{
-    //   console.log(res);
-    //   this.setState({
-    //     list: res.data.splice(0,5)
-    //   })
-    // }).catch((e)=>{
-    //   console.log(e);
-    // })
-  }
-  render() {
-    return (
-      <div className="ineer-container">
-        <h1>To Do List</h1>
-        <section className="input-section">
-          <div className="input-box">
-            <input
-              type="text"
-              placeholder="Enter a new task"
-              className="task-input"
-              onChange={(event) => {
-                this.setState({ value: event.target.value });
-              }}
-              value={this.state.value}
-            ></input>
-            {this.state.error ? <span>{this.state.error}</span> : null}
-          </div>
-          <Button
-            className="btn"
-            title="Add"
-            handleClick={() => {
-              if (this.state.value) {
-                const updatedArr = [
-                  { id: uuidv4(), title: this.state.value },
-                  ...this.state.list,
-                ];
-                this.setState({ list: updatedArr, value: "", error: "" });
-              } else {
-                this.setState({ error: "Plase enter task name " });
-              }
+  }, []);
+  return (
+    <div className="ineer-container">
+      <h1>To Do List</h1>
+      <section className="input-section">
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Enter a new task"
+            className="task-input"
+            onChange={(event) => {
+              setValue(event.target.value);
             }}
-          ></Button>
-        </section>
-        <section className="item-list">
-          {this.state.list.length ? (
-            this.state.list.map((item) => (
-              <ItemList
-                name={item.title}
-                key={item.id}
-                handleDelete={() => {
-                  const filteredItems = this.state.list.filter(
-                    (items) => items.id != item.id
-                  );
-                  this.setState({
-                    list: filteredItems,
-                  });
-                }}
-              />
-            ))
-          ) : (
-            <span>Loading .... </span>
-          )}
-        </section>
-      </div>
-    );
-  }
+            value={value}
+          ></input>
+          {error ? <span>{error}</span> : null}
+        </div>
+        <Button
+          className="btn"
+          title="Add"
+          handleClick={() => {
+            if (value) {
+              const updatedArr = [{ id: uuidv4(), title: value }, ...list];
+              // setState({ ...state, list: updatedArr, value: "", error: "" });
+              setList(updatedArr);
+              setError("");
+              setValue("");
+            } else {
+              setError("Plase enter task name ");
+            }
+          }}
+        ></Button>
+      </section>
+      <section className="item-list">
+        {list?.length ? (
+          list.map((item) => (
+            <ItemList
+              name={item.title}
+              key={item.id}
+              handleDelete={() => {
+                const filteredItems = list.filter(
+                  (items) => items.id != item.id
+                );
+                setList(filteredItems);
+              }}
+            />
+          ))
+        ) : (
+          <span>Loading .... </span>
+        )}
+      </section>
+    </div>
+  );
+
+  // state = {
+  //   value: "",
+  //   list: [],
+  //   error: "",
+  // };
+
+  // async = promis
+  // by async
+
+  //by promis
+
+  // axios.get('https://jsonplaceholder.typicode.com/todos').then(res=>{
+  //   console.log(res);
+  //   this.setState({
+  //     list: res.data.splice(0,5)
+  //   })
+  // }).catch((e)=>{
+  //   console.log(e);
+  // })
 }
 
 export default Home;
